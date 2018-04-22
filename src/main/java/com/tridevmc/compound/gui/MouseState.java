a1aabd753fce3284f21a588076eef4c99e71cbee
Benjamin K <bk1325@gmail.com>
2018-04-22 14:22:25 -0700

    Update code style to modified google, add codestyle.xml

--- src/main/java/com/tridevmc/compound/gui/MouseState.java
+++ src/main/java/com/tridevmc/compound/gui/MouseState.java
@@ -1,18 +1,19 @@
 package com.tridevmc.compound.gui;
 
 public class MouseState {
-	public final int x, y;
-	public final int mouseButton;
 
-	public MouseState(int x, int y) {
-		this.x = x;
-		this.y = y;
-		this.mouseButton = -1;
-	}
+    public final int x, y;
+    public final int mouseButton;
 
-	public MouseState(int x, int y, int mouseButton) {
-		this.x = x;
-		this.y = y;
-		this.mouseButton = mouseButton;
-	}
+    public MouseState(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.mouseButton = -1;
+    }
+
+    public MouseState(int x, int y, int mouseButton) {
+        this.x = x;
+        this.y = y;
+        this.mouseButton = mouseButton;
+    }
 }
--- src/main/java/com/tridevmc/compound/gui/widget/WidgetTest.java
+++ src/main/java/com/tridevmc/compound/gui/widget/WidgetTest.java
@@ -1,36 +1,38 @@
 package com.tridevmc.compound.gui.widget;
 
 import net.minecraft.client.gui.Gui;
-import net.minecraft.client.renderer.GlStateManager;
 
 public class WidgetTest extends WidgetBase {
 
-	int current = 0;
-	int currentTimer = 20;
-	@Override
-	public void drawForeground(int mouseX, int mouseY) {
+    int current = 0;
+    int currentTimer = 20;
+
+    @Override
+    public void drawForeground(int mouseX, int mouseY) {
 //		this.parent.drawBeveledBoxes(24,24,18,18, 9, 4);
 
-		for(int i =0;i < 4;i++) {
-			for(int j = 0;j < 9;j++) {
-				this.parent.drawBeveledBox(24 + j*(18), 24 + (i*18), 18, 18);
-				if((i*9)+j == current) {
-					//GlStateManager.disableLighting();
-					//GlStateManager.disableDepth();
-					//GlStateManager.colorMask(true, true, true, false);
-					Gui.drawRect(24 + j*(18) + 1, 24 + i*(18), 24 + j*(18) + 17, 24 + i*(18) + 17, -2130706433);
-					//GlStateManager.colorMask(true, true, true, true);
-					//GlStateManager.enableLighting();
-					//GlStateManager.enableDepth();
-					currentTimer--;
-					if(currentTimer == 0) {
-						currentTimer = 5;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 9; j++) {
+                this.parent.drawBeveledBox(24 + j * (18), 24 + (i * 18), 18, 18);
+                if ((i * 9) + j == current) {
+                    //GlStateManager.disableLighting();
+                    //GlStateManager.disableDepth();
+                    //GlStateManager.colorMask(true, true, true, false);
+                    Gui.drawRect(24 + j * (18) + 1, 24 + i * (18), 24 + j * (18) + 17,
+                        24 + i * (18) + 17, -2130706433);
+                    //GlStateManager.colorMask(true, true, true, true);
+                    //GlStateManager.enableLighting();
+                    //GlStateManager.enableDepth();
+                    currentTimer--;
+                    if (currentTimer == 0) {
+                        currentTimer = 5;
 
-						if (++current == 4 * 9)
-							current = 0;
-					}
-				}
-			}
-		}
-	}
+                        if (++current == 4 * 9) {
+                            current = 0;
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
