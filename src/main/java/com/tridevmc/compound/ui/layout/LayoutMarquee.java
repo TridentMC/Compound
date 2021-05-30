3a25b9736f09819eaeb0e2c86fe252a808868bd7
Benjamin K <bk1325@gmail.com>
2021-05-29 19:16:02 -0700

    Add vertical movement to the marquee layout - because I can.

--- src/main/java/com/tridevmc/compound/ui/layout/LayoutMarquee.java
+++ src/main/java/com/tridevmc/compound/ui/layout/LayoutMarquee.java
@@ -27,34 +27,50 @@ import com.tridevmc.compound.ui.screen.IScreenContext;
  */
 public class LayoutMarquee implements ILayout {
     private long lastTick = -1;
-    private float lastPosition, currentPosition;
-    private float movementSpeed = 1F;
+    private float lastHorizontalPosition, currentHorizontalPosition;
+    private float lastVerticalPosition, currentVerticalPosition;
+    private float horizontalSpeed = 1F, verticalSpeed = 0F;
 
     public LayoutMarquee() {
     }
 
-    public LayoutMarquee(float movementSpeed) {
-        this.movementSpeed = movementSpeed;
+    public LayoutMarquee(float horizontalSpeed, float verticalSpeed) {
+        this.horizontalSpeed = horizontalSpeed;
+        this.verticalSpeed = verticalSpeed;
+    }
+
+    @Deprecated // Use the version with both horizontal and vertical speeds
+    public LayoutMarquee(float horizontalSpeed) {
+        this.horizontalSpeed = horizontalSpeed;
     }
 
     @Override
     public Rect2F getTransformedRect(IScreenContext screen, IElement element, Rect2F rect) {
         if (screen.getTicks() != this.lastTick) {
             this.lastTick = screen.getTicks();
-            this.updatePosition(this.currentPosition + this.movementSpeed);
+            this.updatePosition();
         }
 
-        if (this.currentPosition > screen.getWidth()) {
-            this.currentPosition = -rect.getWidth();
-            this.lastPosition = this.currentPosition - this.movementSpeed;
+        if (this.currentHorizontalPosition > screen.getWidth()) {
+            this.currentHorizontalPosition = -rect.getWidth();
+            this.lastHorizontalPosition = this.currentHorizontalPosition - this.horizontalSpeed;
         }
 
-        float x = this.lastPosition + ((this.currentPosition - this.lastPosition) * screen.getPartialTicks());
-        return rect.setPosition(x, rect.getY());
+        if (this.currentVerticalPosition > screen.getHeight()) {
+            this.currentVerticalPosition = -rect.getHeight();
+            this.lastVerticalPosition = this.currentVerticalPosition - this.verticalSpeed;
+        }
+
+        float x = this.lastHorizontalPosition + ((this.currentHorizontalPosition - this.lastHorizontalPosition) * screen.getPartialTicks());
+        float y = this.lastVerticalPosition + ((this.currentVerticalPosition - this.lastVerticalPosition) * screen.getPartialTicks());
+        return rect.setPosition(x, y);
     }
 
-    private void updatePosition(float newPosition) {
-        this.lastPosition = this.currentPosition;
-        this.currentPosition = newPosition;
+    private void updatePosition() {
+        this.lastHorizontalPosition = this.currentHorizontalPosition;
+        this.currentHorizontalPosition = this.currentHorizontalPosition + this.horizontalSpeed;
+        this.lastVerticalPosition = this.currentVerticalPosition;
+        this.currentVerticalPosition = this.currentVerticalPosition + this.verticalSpeed;
     }
+
 }
